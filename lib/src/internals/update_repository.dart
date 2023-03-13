db360a05617dea7aae84c9ebbd6fafa50bd6776d
khieunguyen95 <khieunguyen95@gmail.com>
2023-03-13 08:47:42 +0700

    Update update_repository.dart

--- lib/src/internals/update_repository.dart
+++ lib/src/internals/update_repository.dart
@@ -1,16 +1,32 @@
+import 'package:postgres/postgres.dart';
 import 'base_repository.dart';
 
 abstract class ModelRepositoryUpdate<UpdateRequest> {
-  Future<void> updateOne(UpdateRequest request);
-  Future<void> updateMany(List<UpdateRequest> requests);
+  Future<PostgreSQLResult?> updateOne(UpdateRequest request);
+  Future<PostgreSQLResult?> updateMany(List<UpdateRequest> requests);
 }
 
 mixin RepositoryUpdateMixin<UpdateRequest> on BaseRepository
     implements ModelRepositoryUpdate<UpdateRequest> {
   @override
-  Future<void> updateOne(UpdateRequest request) => transaction(() => update([request]));
+  Future<PostgreSQLResult?> updateOne(UpdateRequest request) => transaction(() {
+        try {
+          return update([request]);
+        } catch (e) {
+          return null;
+        }
+      });
   @override
-  Future<void> updateMany(List<UpdateRequest> requests) => transaction(() => update(requests));
+  Future<PostgreSQLResult?> updateMany(List<UpdateRequest> requests) =>
+      transaction(
+        () {
+          try {
+            return update(requests);
+          } catch (e) {
+            return null;
+          }
+        },
+      );
 
-  Future<void> update(List<UpdateRequest> requests);
+  Future<PostgreSQLResult?> update(List<UpdateRequest> requests);
 }
